8abff253f2676b288c8c507525d8aac595294392
jgnam2020 <jgnam@gkn.co.kr>
2020-05-10 14:57:36 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -16,9 +16,21 @@ fi
 echo ---------------------------------------------------------------------------
 
 
-echo -s2----vagrant share folder check------------------------------------------
-vagrant ssh p1 -c'touch /vagrant'
-vagrant ssh p1 -c'touch /vagrant/VagrantShareFolderOK'
+echo -s1----vagrant share folder check------------------------------------------
+vagrant ssh s1 -c'touch /vagrant'
+vagrant ssh s1 -c'touch /vagrant/VagrantShareFolderOK'
 ls |grep VagrantShareFolderOK
 rm VagrantShareFolderOK
+echo ---------------------------------------------------------------------------
+
+echo -s2----vagrant share folder check------------------------------------------
+vagrant ssh s2 -c'systemctl  --no-pager status grafana-server'
+curl http://172.17.8.12:3000|head -n 3
+x=$(which open)
+if [ ${#x} -eq 0 ]; then
+  start http://172.17.8.12:3000
+else
+  open http://172.17.8.12:3000
+fi
+
 echo ---------------------------------------------------------------------------
