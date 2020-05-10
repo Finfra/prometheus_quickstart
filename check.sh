d44a17d573228e7312e26a63f9d147c86dcf0b8d
jgnam2020 <jgnam@gkn.co.kr>
2020-05-10 16:05:08 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -21,6 +21,8 @@ vagrant ssh s1 -c'touch /vagrant'
 vagrant ssh s1 -c'touch /vagrant/VagrantShareFolderOK'
 ls |grep VagrantShareFolderOK
 rm VagrantShareFolderOK
+
+vagrant ssh s1 -c'python3 -c "import prometheus_client;print(prometheus_client.__name__)"'
 echo ---------------------------------------------------------------------------
 
 echo -s2----vagrant share folder check------------------------------------------
